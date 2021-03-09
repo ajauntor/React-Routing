4ee11b14ebb53effd69095619ee7c71786ac01c1
AJ Auntor <auntorknocks@gmail.com>
2021-03-10 02:01:05 +0600

    Set route link

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Friend = (props) => {
-    const {name, email} = props.friend;
+    const {name, email, id} = props.friend;
     return (
         <div>
             <h3>Name : {name}</h3>
             <h5>Email : {email}</h5>
+            <h5>ID: <Link to="/about">Show Details {id}</Link></h5>
         </div>
     );
 };
