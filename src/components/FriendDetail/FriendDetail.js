a80233ae70b4abdae2b1a5e9a4302c2b8ee0d63f
AJ Auntor <auntorknocks@gmail.com>
2021-03-10 02:41:27 +0600

    Read route using useParams()

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
+    const {friendId} = useParams();
     return (
         <div>
-            <h1>This is friend details components</h1> 
+            <h1>This is friend details components {friendId}</h1> 
         </div>
     );
 };
