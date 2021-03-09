da9c83c0b55ddd7ce83d2ed29eea93c291c63029
AJ Auntor <auntorknocks@gmail.com>
2021-03-10 02:14:46 +0600

    Remove Link from app.js Import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Home from "./components/Home/Home";
 import NoMatch from "./components/NoMatch/NoMatch";
